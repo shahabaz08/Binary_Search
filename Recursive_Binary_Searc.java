739af658ef78ed3bc81cfef236ff38db29c9473f
shahabaz08 <139754651+shahabaz08@users.noreply.github.com>
2024-09-10 02:30:40 -0700

    Update Recursive_Binary_Searc.java

--- Recursive_Binary_Searc.java
+++ Recursive_Binary_Searc.java
@@ -61,11 +61,3 @@ class Recursive_Binary_Search {
         }
     }
 }
-
-echo "# binary_Search" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shahabaz08/Binary_Search.git
-git push -u origin main
